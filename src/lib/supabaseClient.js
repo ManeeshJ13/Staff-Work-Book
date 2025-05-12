a1db16c96f4a2c40964f4db257988b502be0c06d
Maneesh James <maneeshjamesa@gmail.com>
2025-05-12 09:24:44 +0530

    Ready to deplay onto vercel

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://pklyecpjduhdqebnvknt.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBrbHllY3BqZHVoZHFlYm52a250Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDMzNDcyODMsImV4cCI6MjA1ODkyMzI4M30.DMZeos9CJuKYbeDUpDixpO5TwyQR-pT-g3Ex8KhsItc';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
 
 
 
